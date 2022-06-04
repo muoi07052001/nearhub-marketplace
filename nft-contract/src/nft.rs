d9e6fbeda39bf304c0142438efbc069199c68d16
Nguyen Duong <muoi07052001@gmail.com>
2022-06-04 09:51:49 +0700

    Update validate for nft_mint

--- nft-contract/src/nft.rs
+++ nft-contract/src/nft.rs
@@ -19,19 +19,51 @@ impl NFTContract {
         receiver_id: AccountId,
     ) {
         let token_id = self.tokens_by_id.len() as u32;
-        let collection_name = self
+
+        // Check token_id đã tồn tại chưa
+        assert!(
+            self.tokens_by_id.get(&token_id).is_none(),
+            "Token id already exists"
+        );
+
+        // Check collection_id có tồn tại không
+        // Lấy collection name từ id
+        let collection = self
             .collections_by_id
             .get(&collection_id)
-            .expect("Collection does not exists")
-            .collection_name;
-        let schema_name = self
+            .expect("Collection does not exists");
+        let collection_name = collection.collection_name;
+
+        // Check schema_id có tồn tại không
+        // Lấy schema name từ id
+        let schema = self
             .schemas_by_id
             .get(&schema_id)
-            .expect("Schema does not exists")
-            .schema_name;
+            .expect("Schema does not exists");
+        let schema_name = schema.schema_name;
+
+        // Check template_id có tồn tại không
+        // Lấy template name từ id
+        let template = self
+            .templates_by_id
+            .get(&template_id)
+            .expect("Template does not exists");
+        let _template_name = template.schema_name;
+
+        // Check xem schema_id đó có thuộc collection_id đó không
+        assert_eq!(
+            schema.collection_name, collection_name,
+            "Schema does not belongs to this collection"
+        );
+        // Check xem template_id đó có thuộc schema_id đó không
+        assert_eq!(
+            template.collection_name, schema_name,
+            "Template does not belongs to this schema"
+        );
 
         let before_storage_usage = env::storage_usage(); // Dùng để tính toán lượng near thừa khi deposit
 
+        // Tạo NFT mới
         let token = Token {
             owner_id: receiver_id,
             token_id,
@@ -73,5 +105,49 @@ impl NFTContract {
         let after_storage_usage = env::storage_usage();
         // Refund NEAR
         refund_deposit(after_storage_usage - before_storage_usage);
+    }
+
+    // Lấy thông tin 1 token dưới dạng JsonToken
+    pub fn nft_token(&self, token_id: TokenId) -> Option<JsonToken> {
+        let token = self.tokens_by_id.get(&token_id);
+
+        if let Some(token) = token {
+            let metadata = self.token_metadata_by_id.get(&token_id).unwrap();
+
+            Some(JsonToken {
+                owner_id: token.owner_id,
+                token_id,
+                collection_id: token.collection_id,
+                collection_name: token.collection_name,
+                schema_id: token.schema_id,
+                schema_name: token.schema_name,
+                template_id: token.template_id,
+                metadata,
+                approved_account_ids: token.approved_account_ids,
+            })
+        } else {
+            None
+        }
+    }
+
+    // Lấy tổng số token đang có trong contract
+    pub fn nft_total_supply(&self) -> U128 {
+        // Đếm tổng số lượng id đang có trong token_metadata_by_id
+        U128(self.token_metadata_by_id.len() as u128)
+    }
+
+    // Lấy danh sách token (có pagination)
+    pub fn nft_tokens(&self, from_index: Option<U128>, limit: Option<u64>) -> Vec<JsonToken> {
+        let collection_keys = self.token_metadata_by_id.keys_as_vector();
+
+        let start = u128::from(from_index.unwrap_or(U128(0)));
+
+        // Duyệt tất cả các keys -> Trả về JsonToken
+        collection_keys
+            .iter()
+            .skip(start as usize) // Pagination
+            .take(limit.unwrap_or(0) as usize) // Pagination
+            .map(|token_id| self.nft_token(token_id.clone()).unwrap())
+            .collect()
     }
 }
