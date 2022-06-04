39470756c0ac3bd563288405e69f8143e761800a
Nguyen Duong <muoi07052001@gmail.com>
2022-06-04 11:25:31 +0700

    Update mint

--- nft-contract/src/nft.rs
+++ nft-contract/src/nft.rs
@@ -89,18 +89,6 @@ impl NFTContract {
         // Thêm token vào danh sách sở hữu bởi owner
         self.internal_add_token_to_owner(&token_id, &token.owner_id);
 
-        // // ----- NFT MINT LOG -----
-        // let nft_mint_log: EventLog = EventLog {
-        //     standard: "nep171".to_string(),
-        //     version: "1.0.0".to_string(),
-        //     event: EventLogVariant::NftMint(vec![NftMintLog {
-        //         owner_id: token.owner_id.to_string(),
-        //         token_ids: vec![token_id.to_string()],
-        //         memo: None,
-        //     }]),
-        // };
-        // env::log(&nft_mint_log.to_string().as_bytes());
-
         // Luợng data storage sử dụng = after_storage_usage - before_storage_usage
         let after_storage_usage = env::storage_usage();
         // Refund NEAR
